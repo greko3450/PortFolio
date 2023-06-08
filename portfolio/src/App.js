566efae49a054ebdbb9372f6a640a29dd1490025
greko3450 <johannsm03@gmail.com>
2023-06-07 19:25:24 -0500

    portfolio

---
+++ portfolio/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import PortFolio from './components/PortFolio.jsx';
+
+function App() {
+  return (
+    <div className="App">
+      <PortFolio/>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ portfolio/src/components/PortFolio.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function PortFolio() {
+	return(
+		<div>hola mundo </div>
+	)
+}
+
+export default PortFolio;
